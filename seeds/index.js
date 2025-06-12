2bf4a5fbc4898c0769adeef4d785444a24180f16
Nikunj Singla <nikunjsingla2005@gmail.com>
2025-06-12 14:05:06 +0530

    sec41done

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,13 @@ const seedDB = async()=>{
 
     for(let i=0;i<50;i++){
         const random1000 = Math.floor(Math.random()*1000);
+        const price = Math.floor(Math.random()*20)+10;
         const camp = new Campground({
-            location: `${cities[random1000].city},${cities[random1000].state}`,
-            title   : `${sample(descriptors)} ${sample(places)}`
+            location      : `${cities[random1000].city},${cities[random1000].state}`,
+            title         : `${sample(descriptors)} ${sample(places)}`,
+            image         : `https://picsum.photos/400?random=${Math.random()}`,
+            description   : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, velit cupiditate voluptatem pariatur aliquam alias molestias dolore corrupti iure esse sed atque vel quis, nam impedit maiores. Possimus, placeat neque! ',
+            price     
             
         })
         await camp.save();
