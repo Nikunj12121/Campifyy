d9e1d502a77e3ce31154eb77f04563b0d5ed9215
Nikunj Singla <nikunjsingla2005@gmail.com>
2025-06-14 16:01:56 +0530

    sec51done

--- app.js
+++ app.js
@@ -7,8 +7,15 @@ const methodOverride = require('method-override');
 const session        = require('express-session');
 const flash  =  require('connect-flash');
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
+const userRoutes = require('./routes/users');
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User  =  require('./models/user');
+
+ 
 
 mongoose.connect('mongodb://localhost:27017/campify_final',{
  //   useNewUrlParser : true,
@@ -46,15 +53,27 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.use((req,res,next)=>{
+    console.log(req.session);
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error   = req.flash('error');
     next();
 })
 
-app.use('/campgrounds',campgrounds);
-app.use('/campgrounds/:id/reviews',reviews);
+ 
+
+app.use('/campgrounds',campgroundRoutes);
+app.use('/campgrounds/:id/reviews',reviewRoutes);
+app.use('/',userRoutes);
 
 
 app.get('/',(req,res)=>{
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,7 +4,7 @@ const catchAsync = require('../utils/catchAsync.js');
 const ExpressError = require('../utils/ExpressError.js');
 const  { campgroundSchema  } = require('../schemas.js');
 const Campground = require('../models/campground.js');
-
+const {isLoggedIn} = require('../middleware.js');
 
 const validateCampground = (req,res,next)=>{
 
@@ -24,13 +24,14 @@ router.get('/',catchAsync(async(req,res)=>{
     res.render('campgrounds/index',{campgrounds});
 }));
 
-router.get('/new',(req,res)=>{
+router.get('/new',isLoggedIn,(req,res)=>{
+
     res.render('campgrounds/new');
 })
 
 
 
-router.post('/',validateCampground, catchAsync(async(req,res)=>{
+router.post('/',isLoggedIn,validateCampground, catchAsync(async(req,res)=>{
     // if(req.body.campground) throw new ExpressError('Invalid Campground Data',400);
 
     
@@ -50,7 +51,7 @@ router.get('/:id',catchAsync(async(req,res)=>{
     res.render('campgrounds/show',{ campground });
 }));
 
-router.get('/:id/edit',async(req,res)=>{
+router.get('/:id/edit',isLoggedIn,async(req,res)=>{
     const campground = await Campground.findById(req.params.id);
         if(!campground){
         req.flash('error','Cannot find that campground');
@@ -59,14 +60,14 @@ router.get('/:id/edit',async(req,res)=>{
     res.render('campgrounds/edit',{ campground });
 })
 
-router.put('/:id',validateCampground,catchAsync(async(req,res)=>{
+router.put('/:id',isLoggedIn,validateCampground,catchAsync(async(req,res)=>{
     const {id} = req.params;
   const campground = await Campground.findByIdAndUpdate(id,{ ...req.body.campground});
     req.flash('success','Successfully updated campground');
   res.redirect(`/campgrounds/${campground._id}`);
 }));
 
-router.delete('/:id',catchAsync(async(req,res)=>{
+router.delete('/:id',isLoggedIn,catchAsync(async(req,res)=>{
     const {id} = req.params;
     await Campground.findByIdAndDelete(id);
     req.flash('success','Successfully deleted campground');
