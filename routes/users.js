8ff529b5840fdedf044a52735a9188c117c8a9ce
Nikunj Singla <nikunjsingla2005@gmail.com>
2025-06-15 00:28:07 +0530

    sec53done

--- routes/users.js
+++ routes/users.js
@@ -5,48 +5,18 @@ const User    = require('../models/user');
 const { remove } = require('../models/user');
 const passport = require('passport');
 const { storeReturnTo } = require('../middleware');
-router.get('/register',(req,res)=>{
-    res.render('users/register');
-});
-router.post('/register',catchAsync(async(req,res,next)=>{
-    try{
-    const { email, username , password } = req.body;
-    const user = new User({ email,username});
-    const registeredUser = await User.register(user,password);
-    req.login(registeredUser,err => {
-        if(err) return next(err);
-    // console.log(registeredUser);
-    req.flash('success','Welcome to Campify!');
-    res.redirect('/campgrounds');
-    });
+const users = require('../controllers/users');
 
-}catch(e){
-    req.flash('error',e.message);
-    res.redirect('register');
-}
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register))
+
     
-}));
+router.route('/login')
+    .get(users.renderLogin)
+    .post(storeReturnTo,passport.authenticate('local',{failureFlash:true,failureRedirect:'/login'}),users.login)
+    
 
-
-router.get('/login',(req,res)=>{
-    res.render('users/login');
-})
-
-router.post('/login',storeReturnTo,passport.authenticate('local',{failureFlash:true,failureRedirect:'/login'}),(req,res)=>{
-    req.flash('success','Welcome Back!');
-  const redirectUrl = res.locals.returnTo || '/campgrounds';
-    delete req.session.returnTo;
-    res.redirect(redirectUrl);
-})
-
-router.get('/logout', (req, res, next) => {
-    req.logout(function (err) {
-        if (err) {
-            return next(err);
-        }
-        req.flash('success', 'Goodbye!');
-        res.redirect('/campgrounds');
-    });
-}); 
+router.get('/logout',users.logout); 
 
 module.exports = router;
