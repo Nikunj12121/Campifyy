ca21baa3d073400eb8ef055ff5911e96decae881
Nikunj Singla <nikunjsingla2005@gmail.com>
2025-06-15 14:19:30 +0530

    sec54done

--- seeds/index.js
+++ seeds/index.js
@@ -27,9 +27,25 @@ const seedDB = async()=>{
             author        : '684d29c3bdd5d173e4b9acca',
             location      : `${cities[random1000].city},${cities[random1000].state}`,
             title         : `${sample(descriptors)} ${sample(places)}`,
-            image         : `https://picsum.photos/400?random=${Math.random()}`,
             description   : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, velit cupiditate voluptatem pariatur aliquam alias molestias dolore corrupti iure esse sed atque vel quis, nam impedit maiores. Possimus, placeat neque! ',
-            price     
+            price,
+            images        :  [
+    {
+      url: 'https://res.cloudinary.com/dk1migr8k/image/upload/v1749972920/Campify/kl0gom8gvjavqaorhs7o.jpg',
+      filename: 'Campify/kl0gom8gvjavqaorhs7o',
+      
+    },
+    {
+      url: 'https://res.cloudinary.com/dk1migr8k/image/upload/v1749972924/Campify/d2dgujmfmd9aaxh37oak.jpg',
+      filename: 'Campify/d2dgujmfmd9aaxh37oak',
+      
+    },
+    {
+      url: 'https://res.cloudinary.com/dk1migr8k/image/upload/v1749972925/Campify/yoizt30fkta0eyawsrm7.jpg',
+      filename: 'Campify/yoizt30fkta0eyawsrm7',
+      
+    }
+  ]
             
         })
         await camp.save();
