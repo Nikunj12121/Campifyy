adf6752cc957b3304cd2d7a2b54d8590c601739a
Nikunj Singla <nikunjsingla2005@gmail.com>
2025-06-15 18:46:29 +0530

    last

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const { MongoStore } = require('connect-mongo');
 const MongoDBStore = require("connect-mongo")(session);
 
 //  const dbUrl = 'mongodb://localhost:27017/campify_final';
- const dbUrl = process.env.DB_Url || 'mongodb://localhost:27017/campify_final';
+ const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/campify_final';
 
 // 'mongodb://localhost:27017/campify_final'
 mongoose.connect(dbUrl,{
